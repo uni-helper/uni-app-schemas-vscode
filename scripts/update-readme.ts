a911289dfe7fc9d36e9b259af87bfc66b249c4a4
ModyQyW <wurui.dev@gmail.com>
2024-04-09 16:39:27 +0800

    chore: lint

--- scripts/update-readme.ts
+++ scripts/update-readme.ts
@@ -1,5 +1,5 @@
 import { writeFileSync } from 'node:fs';
-import { resolve, dirname } from 'node:path';
+import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { simpleGit } from 'simple-git';
 
