61391b43f05eef10fed2643aff5a667c3ec666cf
Panpan <wurui.dev@gmail.com>
2026-09-05 19:59:20 +0900

    chore: remove simple-git, update devEngines

--- scripts/update-readme.ts
+++ scripts/update-readme.ts
@@ -1,7 +1,6 @@
 import { writeFileSync } from "node:fs";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import { simpleGit } from "simple-git";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const root = resolve(__dirname, "..");
@@ -85,5 +84,3 @@ const readme = `# @uni-helper/uni-app-schemas-vscode
 `;
 
 writeFileSync(readmePath, readme);
-
-await simpleGit().add(readmePath);
